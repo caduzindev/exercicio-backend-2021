128e2e9bd64e7179e6fdc5424f4a2eb932788988
root <root@DESKTOP-KR2HEJ1.localdomain>
2021-06-14 16:08:01 -0300

    Adicionado hook de theme

--- web/modules/textwrap/src/Controller/TextWrapController.php
+++ web/modules/textwrap/src/Controller/TextWrapController.php
@@ -7,7 +7,7 @@ use Drupal\Core\Controller\ControllerBase;
 class TextWrapController extends ControllerBase {
   public function content() {
     $build = [
-      '#markup' => $this->t('Hello World!'),
+      '#theme'=>'textWrap',
     ];
     return $build;
   }
