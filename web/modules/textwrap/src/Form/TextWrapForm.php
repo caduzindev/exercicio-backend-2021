348a193ed22afb6fee4e8ef01f0d475a4953244c
root <root@DESKTOP-KR2HEJ1.localdomain>
2021-06-15 13:43:27 -0300

    Projeto quase pronto

--- web/modules/textwrap/src/Form/TextWrapForm.php
+++ web/modules/textwrap/src/Form/TextWrapForm.php
@@ -38,14 +38,17 @@ class TextWrapForm extends FormBase{
   /**
    * {@inheritdoc}
    */
-  public function validateForm(array &$form, FormStateInterface $form_state) {}
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    if(intval($form_state)<=0){
+      $form_state->setErrorByName('number','Campo com tamanho invalido');
+    }
+  }
 
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $text = $form_state->getValue('text');
     $numberOfLines = intval($form_state->getValue('number'));
     $textwrap = new TextWrap();
     $textBroken = $textwrap->wrap($text,$numberOfLines);
-    // $textwrap = (new \Drupal\textwrap\TexWrap)->wrap($text,$numberOfLines);
 
     \Drupal::database()
       ->insert('texts')
@@ -54,5 +57,7 @@ class TextWrapForm extends FormBase{
         'brokenText'=>implode('&',$textBroken)
       ])
       ->execute();
+    
+    $this->messenger()->addStatus($this->t('Seu texto fica assim =><br/> '.implode('<br/>',$textBroken)));
   }
 }
